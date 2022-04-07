d71dcaf8b61de3e3581138f91c4d4dd688bfdaac
Yuri Kuivjogi Fernandes <yurikfernandes@gmail.com>
2022-04-07 13:12:56 -0300

    add links to admin crud

--- teste/delivery/admin.py
+++ teste/delivery/admin.py
@@ -5,11 +5,15 @@ from .models import Loja, Motoboy, Pedido
 @admin.register(Loja)
 class LojaAdmin(admin.ModelAdmin):
     list_display = ['id', 'comissao']
+    list_display_links = ['id', 'comissao']
 
 @admin.register(Motoboy)
 class MotoboyAdmin(admin.ModelAdmin):
     list_display = ['id', 'valor_fixo', 'prioridade_loja']
+    list_display_links = ['id', 'valor_fixo', 'prioridade_loja']
+    
 
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
     list_display = ['id', 'valor', 'loja', 'motoboy']
+    list_display_links = ['id', 'valor', 'loja', 'motoboy']
